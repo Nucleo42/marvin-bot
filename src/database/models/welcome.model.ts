924fa3ecbe4d765bbf91599d94fc24a145ad03f9
parlandin <56051040+parlandin@users.noreply.github.com>
2025-01-10 23:07:23 -0300

    feat: add WelcomeModel for managing guild welcome channel settings

---
+++ src/database/models/welcome.model.ts
@@ -0,0 +1,43 @@
+import { guildWelcomeChannel } from "../schema";
+import { database } from "../../index";
+import { eq } from "drizzle-orm";
+
+export interface WelcomeChannel {
+  guildID: string;
+  channelID: string;
+  enabled: boolean;
+  rulesChannel?: string;
+  presentationChannel?: string;
+}
+
+export class WelcomeModel {
+  async setWelcomeChannel({
+    guildID,
+    channelID,
+    enabled,
+    presentationChannel,
+    rulesChannel,
+  }: WelcomeChannel) {
+    const welcomeChannel: typeof guildWelcomeChannel.$inferInsert = {
+      guild_id: guildID,
+      channel_id: channelID,
+      enabled: enabled,
+      presentationChannel: presentationChannel,
+      rulesChannel: rulesChannel,
+    };
+
+    await database.bd
+      .insert(guildWelcomeChannel)
+      .values(welcomeChannel)
+      .execute();
+  }
+
+  async getWelcomeChannel(guildID: string) {
+    const welcomeChannel = await database.bd
+      .select()
+      .from(guildWelcomeChannel)
+      .where(eq(guildWelcomeChannel.guild_id, guildID));
+
+    return welcomeChannel;
+  }
+}
