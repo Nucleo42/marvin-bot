47eff8ab373aee5466c94d8a6b3f8c68d458b347
parlandin <56051040+parlandin@users.noreply.github.com>
2025-01-13 22:29:54 -0300

    feat: add EventHandler and EventsLoader for dynamic event registration and loading

---
+++ src/infrastructure/discord/handlers/EventHandler.ts
@@ -0,0 +1,47 @@
+import { Logger } from "@logging/logger";
+import { ClientEvents } from "discord.js";
+import { ClientDiscord } from "@discord/client";
+import { injectable, inject } from "tsyringe";
+import { EventTypes } from "@domain/interfaces/events/event";
+
+@injectable()
+export class EventHandler {
+  constructor(
+    @inject(ClientDiscord) private client: ClientDiscord,
+    @inject(Logger) private logger: Logger,
+  ) {}
+
+  public register<K extends keyof ClientEvents>(event: EventTypes<K>): void {
+    const { name, once, execute } = event;
+
+    try {
+      if (once) {
+        this.client.once(name, async (...args: ClientEvents[K]) => {
+          await this.safeExecute(name, execute, ...args);
+        });
+      } else {
+        this.client.on(name, async (...args: ClientEvents[K]) => {
+          await this.safeExecute(name, execute, ...args);
+        });
+      }
+
+      this.logger.info(
+        `O evento '${name}' foi registrado como: ${once ? "once" : "persistently"}`,
+      );
+    } catch (error) {
+      this.logger.error(`Erro ao registrar o evento:'${name}':`, error);
+    }
+  }
+
+  private async safeExecute<K extends keyof ClientEvents>(
+    name: K,
+    handler: (...args: ClientEvents[K]) => Promise<void> | void,
+    ...args: ClientEvents[K]
+  ): Promise<void> {
+    try {
+      await Promise.resolve(handler(...args));
+    } catch (error) {
+      this.logger.error(`Erro ao inciar o handle do evento: '${name}':`, error);
+    }
+  }
+}
