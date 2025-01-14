f49106987dddd783c2493862bfa84e1a8a9b51be
parlandin <56051040+parlandin@users.noreply.github.com>
2025-01-13 22:28:03 -0300

    feat: refactor index.ts to use dependency injection for improved modularity and error handling

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,27 @@
-import { ClientDiscord } from "@structures/ClientDiscord";
-import Logging from "@structures/Logging";
-import { Clientdrizzle } from "@structures/Clientdrizzle";
-import { LevelClient } from "@structures/ClientLevelDB";
+import "reflect-metadata";
+import "@container/index";
+import { container } from "tsyringe";
+import { ClientDiscord } from "@discord/client";
+import { Logger } from "@logging/logger";
+import { EventsLoader } from "@discord/loaders/EventsLoader";
+import { CommandLoader } from "@discord/loaders/CommandLoader";
 
-const client = new ClientDiscord();
-const database = new Clientdrizzle();
-const localDd = new LevelClient("./.local");
+async function bootstrap() {
+  const client = container.resolve(ClientDiscord);
+  const logger = container.resolve(Logger);
 
-database
-  .start()
-  .then(() => {
-    Logging.success("Database connected!");
-  })
-  .catch((err) => Logging.error(err));
+  const eventsLoader = container.resolve(EventsLoader);
+  await eventsLoader.registerEvents();
 
-client
-  .start()
-  .then(() => {
-    Logging.success("Bot started!");
-  })
-  .catch((err) => Logging.error(err));
+  const commandLoader = container.resolve(CommandLoader);
+  await commandLoader.registerCommands();
 
-process.on("unhandledRejection", (reason, promise) => {
   try {
-    console.error(
-      "Unhandled Rejection at: ",
-      promise,
-      "reason: ",
-      (reason instanceof Error ? reason.stack : reason) || reason,
-    );
-  } catch {
-    console.error(reason);
+    await client.start();
+    logger.info("O client do discord foi iniciado!");
+  } catch (error) {
+    logger.error("Ocorreu um erro ao iniciar o client do discord:", error);
   }
-});
+}
 
-export { database, localDd };
-export default client;
+bootstrap();
