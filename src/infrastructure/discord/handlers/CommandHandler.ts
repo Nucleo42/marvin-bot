2e94db2bf2a7bd24ca38faeaec7f265475bb6fc6
parlandin <56051040+parlandin@users.noreply.github.com>
2025-01-13 22:29:36 -0300

    feat: implement CommandHandler and CommandLoader for dynamic command registration and loading

---
+++ src/infrastructure/discord/handlers/CommandHandler.ts
@@ -0,0 +1,38 @@
+import { ApplicationCommandDataResolvable } from "discord.js";
+import { ClientDiscord } from "@discord/client";
+import { injectable, inject } from "tsyringe";
+import { CommandType } from "@interfaces/commands/Command";
+
+@injectable()
+export class CommandHandler {
+  private slashCommands: Array<ApplicationCommandDataResolvable> = [];
+
+  constructor(@inject(ClientDiscord) private client: ClientDiscord) {}
+
+  public getListOfSlashCommands(): Array<ApplicationCommandDataResolvable> {
+    return this.slashCommands;
+  }
+
+  public register(commands: CommandType): void {
+    const { name, buttons, selects, modals } = commands;
+
+    if (name) {
+      this.client.commands.set(name, commands);
+      this.slashCommands.push(commands);
+
+      if (buttons) {
+        buttons.forEach((execute, key) =>
+          this.client.buttons.set(key, execute),
+        );
+      }
+
+      if (selects) {
+        selects.forEach((execute, key) => this.client.select.set(key, execute));
+      }
+
+      if (modals) {
+        modals.forEach((execute, key) => this.client.modals.set(key, execute));
+      }
+    }
+  }
+}
---
+++ src/infrastructure/discord/loaders/CommandLoader.ts
@@ -0,0 +1,72 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { CommandType } from "@interfaces/commands/Command";
+import { inject, injectable } from "tsyringe";
+import { CommandHandler } from "@discord/handlers/CommandHandler";
+import { Logger } from "@logging/logger";
+import { ClientDiscord } from "@discord/client";
+import { isDev } from "@utils/isDev";
+
+@injectable()
+export class CommandLoader {
+  constructor(
+    @inject("CommandsPath") private commandsPath: string,
+    @inject(Logger) private logger: Logger,
+    @inject(CommandHandler) private commandHandler: CommandHandler,
+    @inject(ClientDiscord) private clientDiscord: ClientDiscord,
+  ) {}
+
+  private async loadCommands(): Promise<CommandType[]> {
+    const commands: CommandType[] = [];
+
+    async function readCommandsRecursively(dir: string): Promise<void> {
+      const files = await fs.readdir(dir, { withFileTypes: true });
+
+      for (const file of files) {
+        const fullPath = path.join(dir, file.name);
+
+        if (file.isDirectory()) {
+          if (!isDev && file.name === "development") continue;
+
+          await readCommandsRecursively(fullPath);
+        } else if (file.name.endsWith(".ts") || file.name.endsWith(".js")) {
+          const command = await import(fullPath);
+
+          if (command.default?.name && command.default?.execute) {
+            commands.push(command.default);
+          }
+        }
+      }
+    }
+
+    await readCommandsRecursively(this.commandsPath);
+    return commands;
+  }
+
+  public async registerCommands(): Promise<void> {
+    try {
+      const commands = await this.loadCommands();
+
+      for (const command of commands) {
+        this.commandHandler.register(command);
+
+        this.logger.info(`registrado o comando: ${command.name}`);
+      }
+
+      this.logger.info(`Total de ${commands.length} comandos carregados`);
+      await this.applyCommands();
+    } catch (error) {
+      this.logger.error("Erro ao carregar comandos:", error);
+    }
+  }
+
+  private async applyCommands() {
+    const listOfCommands = this.commandHandler.getListOfSlashCommands();
+
+    this.clientDiscord.on("ready", async () => {
+      await this.clientDiscord.application?.commands.set(listOfCommands);
+
+      this.logger.success("Comandos aplicados com sucesso!");
+    });
+  }
+}
