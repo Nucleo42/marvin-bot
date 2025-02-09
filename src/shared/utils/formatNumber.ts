d8185c02db2cdfc331615800497589e06a7d7b1d
parlandin <56051040+parlandin@users.noreply.github.com>
2025-02-09 15:20:32 -0300

    feat: add utility function to format numbers in K and M

---
+++ src/shared/utils/formatNumber.ts
@@ -0,0 +1,9 @@
+export function formatNumber(number: number): string {
+  if (number >= 1000000) {
+    return (number / 1000000).toFixed(1).replace(".0", "") + "M";
+  }
+  if (number >= 1000) {
+    return (number / 1000).toFixed(1).replace(".0", "") + "K";
+  }
+  return number.toString();
+}
