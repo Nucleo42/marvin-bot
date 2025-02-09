6e5412f2f598d6bf50f2772a0b21d50fb6f322ff
parlandin <56051040+parlandin@users.noreply.github.com>
2025-02-09 14:26:31 -0300

    feat: implement bot status service to update activity on ready event

--- src/shared/container/discord/events.ts
+++ src/shared/container/discord/events.ts
@@ -4,9 +4,11 @@ import { CreateCanvasCardService } from "@services/events/CreateCanvasCardServic
 import { MemberLeaveService } from "@services/events/MemberLeaveService";
 import { IntroductionListenerService } from "@services/events/IntroductionListenerService";
 import { AddMemberOnBanService } from "@services/events/AddMemberOnBanListService";
+import { BotStatusService } from "@services/events/BotStatusService";
 
 container.registerSingleton(MemberWelcomeService, MemberWelcomeService);
 container.registerSingleton(CreateCanvasCardService, CreateCanvasCardService);
 container.registerSingleton(MemberLeaveService, MemberLeaveService);
 container.register(IntroductionListenerService, IntroductionListenerService);
 container.register(AddMemberOnBanService, AddMemberOnBanService);
+container.register(BotStatusService, BotStatusService);
