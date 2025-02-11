115a78682f62d0d6c0c80e2c47f2dc9d3529866c
parlandin <56051040+parlandin@users.noreply.github.com>
2025-02-11 12:41:49 -0300

    feat: add GEMINI configuration to environment variables

--- src/infrastructure/configs/EnvironmentVariables.ts
+++ src/infrastructure/configs/EnvironmentVariables.ts
@@ -7,4 +7,8 @@ export default {
     CRON_TIME: process.env.BAN_JOB_CRON_TIME,
     WAITING_TIME: process.env.BAN_JOB_WAITING_TIME,
   },
+  GEMINI: {
+    URL: process.env.GEMINI_URL,
+    API_KEY: process.env.GEMINI_URL_API_KEY,
+  },
 };
