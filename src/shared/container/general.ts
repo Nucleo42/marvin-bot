fbf59ad3a3337f3f96332c5e1860aed9d0b5da12
parlandin <56051040+parlandin@users.noreply.github.com>
2025-02-11 12:52:03 -0300

    feat: implement AnnouncementProjectService and event listener for thread creation

--- src/shared/container/general.ts
+++ src/shared/container/general.ts
@@ -1,4 +1,8 @@
 import { AdminPermissionService } from "@services/AdminPermissionService";
 import { container } from "tsyringe";
+import { GeminiIA } from "@infrastructure/IA/Gemini";
+import { getProjectSlotsGemini } from "@infrastructure/IA/GetProjectSlotsGemini";
 
 container.register(AdminPermissionService, AdminPermissionService);
+container.register(GeminiIA, GeminiIA);
+container.register(getProjectSlotsGemini, getProjectSlotsGemini);
