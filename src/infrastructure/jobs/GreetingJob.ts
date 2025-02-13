ea5465b646bf3127a41cd51f1d4acc886d8897d2
parlandin <56051040+parlandin@users.noreply.github.com>
2025-02-13 18:45:37 -0300

    feat: adjust GreetingJob schedule and update probability checks for greeting activation

--- src/infrastructure/jobs/GreetingJob.ts
+++ src/infrastructure/jobs/GreetingJob.ts
@@ -26,7 +26,7 @@ export class GreetingJob {
 
   private scheduleTask(): void {
     this.task = cron.schedule(
-      "0 6,14,20,23 * * *",
+      "0 6,14,20 * * *",
       async () => {
         await this.processGreeting();
       },
@@ -121,20 +121,8 @@ export class GreetingJob {
     const { hour } = getDayAndTime();
 
     const random = Math.floor(Math.random() * 100);
-    if (hour > 7 && hour < 23) {
-      if (random < 70) {
-        if (isDev) {
-          this.logger.info({
-            prefix: "greeting-job",
-            message: "Não enviando saudação por conta da probabilidade",
-          });
-        }
-        return;
-      }
-    }
-
-    if (hour >= 23) {
-      if (random < 95) {
+    if (hour > 7) {
+      if (random < 90) {
         if (isDev) {
           this.logger.info({
             prefix: "greeting-job",
