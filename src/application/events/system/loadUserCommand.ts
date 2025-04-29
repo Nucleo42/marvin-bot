81c1b72e3ff8a7eee6e98f8804f8bede74314ed3
parlandin <56051040+parlandin@users.noreply.github.com>
2025-04-29 20:30:52 -0300

    feat: implement interactionCreate event handler for user context menu commands

---
+++ src/application/events/system/loadUserCommand.ts
@@ -0,0 +1,21 @@
+import { Event } from "@interfaces/discord/Event";
+import { CommandInteractionOptionResolver } from "discord.js";
+import { ClientDiscord } from "@discord/Client";
+import { container } from "tsyringe";
+
+export default new Event({
+  name: "interactionCreate",
+  execute: (interaction) => {
+    if (!interaction.isUserContextMenuCommand()) return;
+
+    const client = container.resolve(ClientDiscord);
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    const options = interaction.options as CommandInteractionOptionResolver;
+
+    command.execute({ client, interaction, options });
+  },
+});
